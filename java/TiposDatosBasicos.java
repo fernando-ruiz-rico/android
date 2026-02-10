89f0dd046138566ac1fb275135b3b4fca9b8c7f2
fernando-ruiz-rico <fernando@iessanvicente.com>
2026-02-10 10:37:02 +0100

    Agregar ejemplos adicionales de conversión de tipos en la clase TiposDatosBasicos

--- java/TiposDatosBasicos.java
+++ java/TiposDatosBasicos.java
@@ -3,12 +3,45 @@ public class TiposDatosBasicos {
 
         float pi = 3.1416f;
         int piInteger = (int)pi;
-
         System.out.println(piInteger);
 
         int number = 5;
         double realNumber = (double)number;
-
         System.out.println(realNumber);
+
+        char symbol = 'a';
+        System.out.println(symbol);
+
+        String text = "Hello World";
+        System.out.println(text);
+
+        char symbol2 = 'a';
+        symbol2 += 25;
+        System.out.println(symbol2);
+
+        String text2 = "Hello" + 3 + 2;
+        System.out.println(text2);
+
+        String text3 = "Hello" + (3 + 2);
+        System.out.println(text3);
+
+        float result = (float) 3 / 2;
+        System.out.println(result);
+
+        float a = 3.5f;
+        int b = 4;
+        float result2 = a * b;
+        System.out.println(result2);
+
+        int value = Integer.parseInt("023");
+        System.out.println(value);
+
+        int number2 = 23;
+        String text4 = "" + number2;
+        System.out.println(text4);
+
+        int number3 = 23;
+        String text5 = String.valueOf(number3);
+        System.out.println(text5);
     }
 }
