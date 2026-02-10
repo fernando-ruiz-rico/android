a09d60c121e8398fb71f57ed194a02ec08ccefbe
fernando-ruiz-rico <fernando@iessanvicente.com>
2026-02-10 10:11:33 +0100

    Agregar clase TiposDatosBasicos con ejemplos de conversión de tipos

---
+++ java/TiposDatosBasicos.java
@@ -0,0 +1,14 @@
+public class TiposDatosBasicos {
+    public static void main(String[] args) {
+
+        float pi = 3.1416f;
+        int piInteger = (int)pi;
+
+        System.out.println(piInteger);
+
+        int number = 5;
+        double realNumber = (double)number;
+
+        System.out.println(realNumber);
+    }
+}
