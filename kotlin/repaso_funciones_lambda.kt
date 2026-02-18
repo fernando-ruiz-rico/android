9998797af049881da08f16ca5be415f28e8c888a
Arturo Bernal <arturober@gmail.com>
2026-02-18 09:55:23 +0100

    repaso lambda

--- kotlin/repaso_funciones_lambda.kt
+++ kotlin/repaso_funciones_lambda.kt
@@ -15,6 +15,8 @@ fun operar(n1: Int, n2: Int, operacion: (Int, Int) -> Int): Int {
   return operacion(n1, n2)
 }
 
+data class Persona(var nombre: String, var edad: Int)
+
 fun main() {
   saluda("Pepe")
   val nombres = listOf("Pepe", "Juan", "Marta")
@@ -44,11 +46,37 @@ fun main() {
 
   val palabras = listOf("abeto", "mesa", "gato", "abanico", "aro", "teclado", "abedul", "mano", "aguacate")
   // Imprime las 3 primeras palabras que empiezan por 'a' en mayúsculas
-  println(palabras.filter { it.startsWith("a") }.take(3).map { it.uppercase()})
+  println(palabras.filter { it.startsWith("a") }.take(3).map { it.uppercase() })
 
   // Imprime las longitudes de las cadenas ordenadas de mayor a menor
-  println(palabras.map { it.length }.sortedDescending() )
+  println(palabras.map { it.length }.sortedDescending())
 
   // A partir de una lista de palabras, imprime la concatenación de la primera letra de cada palabra
   // Ejemplo: ["pera", "manzana", "platano"] -> "pmp"
+  println(palabras.map { it[0] }.joinToString(""))
+
+  val persona = Persona("Juan", 23)
+  println(persona)
+  println(persona.nombre) // Juan
+  println(persona.edad) // 23
+
+  val personas = listOf<Persona>(
+    Persona("Juan", 23),
+                                 Persona("Marta", 41),
+                                 Persona("Pedro", 16),
+                                 Persona("Marco", 62),
+                                 Persona("Ana", 15),
+                                 Persona("Martín", 43),
+  )
+
+  // Imprime personas menores de edad
+  println(personas.filter { it.edad < 18 })
+  // Nombres personas menores de edad
+  println(personas.filter { it.edad < 18 }.map { it.nombre })
+  // Suma las edades de los 3 primeros
+  println(personas.take(3).sumOf { it.edad })
+  // Nombres en mayúsculas de las personas que empiezan por M
+  println(personas.map { it.nombre }.filter { it.startsWith("M") }.joinToString() )
+  // 3 personas más mayores
+  println(personas.sortedByDescending { it.edad }.take(3))
 }
