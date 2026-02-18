a790d565142fbde5e1251f164785e4417cdbf5ec
Arturo Bernal <arturober@gmail.com>
2026-02-18 10:52:24 +0100

    Ejemplo evento click

---
+++ Android/EjemploInteraccion/app/src/main/java/com/example/ejemplointeraccion/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.ejemplointeraccion
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ejemplointeraccion.ui.theme.EjemploInteraccionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EjemploInteraccionTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+  var contador by remember { mutableIntStateOf(0) }
+
+  Column(modifier = modifier.padding(horizontal = 12.dp)) {
+    Text(
+      text = "$contador",
+      modifier = Modifier
+    )
+    Button(onClick = { contador++ }) {
+      Text(text = "Incrementar contador")
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EjemploInteraccionTheme {
+        Greeting()
+    }
+}
