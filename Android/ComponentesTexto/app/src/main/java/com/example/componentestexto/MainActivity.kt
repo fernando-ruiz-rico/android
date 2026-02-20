92d6b1171c43d1e7bc035c9d7c4b9de37c396060
Arturo Bernal <arturober@gmail.com>
2026-02-20 09:46:26 +0100

    Campos de texto

--- Android/ComponentesTexto/app/src/main/java/com/example/componentestexto/MainActivity.kt
+++ Android/ComponentesTexto/app/src/main/java/com/example/componentestexto/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.foundation.text.input.insert
+import androidx.compose.foundation.text.input.maxLength
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,6 +29,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.componentestexto.ui.theme.ComponentesTextoTheme
 
@@ -45,6 +55,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
   Column(modifier = modifier) {
     TextFieldClasico()
     TextFieldModerno()
+    TextFieldCompleto()
   }
 }
 
@@ -57,7 +68,8 @@ fun TextFieldClasico() {
     onValueChange = { textValue = it },
     singleLine = true,
     label = { Text("Nombre") },
-    placeholder = { Text("Ingrese su nombre") }
+    placeholder = { Text("Ingrese su nombre") },
+    modifier = Modifier.fillMaxWidth()
   )
   Text(text = textValue)
 }
@@ -70,9 +82,38 @@ fun TextFieldModerno() {
     state = textState,
     lineLimits = TextFieldLineLimits.SingleLine,
     label = { Text("Nombre") },
-    placeholder = { Text("Ingrese su nombre") }
+    placeholder = { Text("Ingrese su nombre") },
+    modifier = Modifier.fillMaxWidth()
   )
   Text(text = "${textState.text}")
+}
+
+@Composable
+fun TextFieldCompleto() {
+  val textState = rememberTextFieldState()
+
+  TextField(
+    state = textState,
+    placeholder = { Text("Caducidad de la tarjeta") },
+    label = { Text("Caducidad") },
+    lineLimits = TextFieldLineLimits.SingleLine,
+    leadingIcon = { Icon(Icons.Default.CalendarMonth, contentDescription = null) },
+    trailingIcon = {
+      if(textState.text.isNotEmpty()) {
+        IconButton(onClick = { textState.clearText() }) {
+          Icon(Icons.Default.Clear, contentDescription = "Clear text")
+        }
+      }
+   },
+    keyboardOptions = KeyboardOptions(
+      keyboardType = KeyboardType.Number
+    ),
+    inputTransformation = InputTransformation.maxLength(4),
+    outputTransformation = {
+      if (length > 2) insert(2, "/") // Separador de mes/año
+    },
+    modifier = Modifier.fillMaxWidth(),
+  )
 }
 
 @Preview(showBackground = true)
