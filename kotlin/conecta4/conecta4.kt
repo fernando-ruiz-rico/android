b386d5877dec7f0fb1f4bd8920a386c18b7f52ec
fernando-ruiz-rico <fernando@iessanvicente.com>
2026-02-24 12:05:00 +0100

    Añadida lógica para el manejo de movimientos de la máquina y verificación de victoria en el juego Conecta 4

--- kotlin/conecta4/conecta4.kt
+++ kotlin/conecta4/conecta4.kt
@@ -1,3 +1,18 @@
+import kotlin.random.Random
+
+/* * Estructura del código RGB: "\u001B[38;2;R;G;Bm"
+* 1. "\u001B"  -> Es el código de "Escape". Le dice a la consola: "¡Atención, viene un comando!"
+* 2. "["       -> Abre el comando.
+* 3. "38;2;"   -> Le indica a la consola que vamos a usar un color de texto en formato RGB.
+* 4. "R;G;B"   -> Son los valores de Rojo (Red), Verde (Green) y Azul (Blue). Van de 0 a 255.
+* 5. "m"       -> Cierra el comando.
+ */
+
+val RESET = "\u001B[0m"
+val ROJO = "\u001B[38;2;255;0;0m"
+val AMARILLO = "\u001B[38;2;255;255;0m"
+val AZUL = "\u001B[38;2;0;100;255m"
+
 enum class Dificultad(val descripcion: String) {
     FACIL("Fácil (Aleatorio)"),
     MEDIO("Medio (Defensivo)"),
@@ -8,8 +23,8 @@ enum class Dificultad(val descripcion: String) {
 
 enum class Ficha(val simbolo: String) {
     VACIO(" . "),
-    JUGADOR(" X "),
-    MAQUINA(" 0 ");
+    JUGADOR("$ROJO X $RESET"),
+    MAQUINA("$AMARILLO 0 $RESET");
 
     override fun toString() : String = simbolo
 }
@@ -21,11 +36,26 @@ const val NO_ENCONTRADO = -1
 
 val tablero = MutableList(FILAS) { MutableList(COLUMNAS) { Ficha.VACIO } }
 
-fun columValida(c: Int): Boolean {
+fun columnaValida(c: Int): Boolean {
     return c >= 0 && c < COLUMNAS && tablero[0][c] == Ficha.VACIO
 }
 
+fun obtenerFilaLibre(c:Int): Int {
+    var filaEncontrada = NO_ENCONTRADO
+
+    for (f in FILAS - 1 downTo 0) {
+        if (filaEncontrada == NO_ENCONTRADO && tablero[f][c] == Ficha.VACIO) {
+            filaEncontrada = f
+        }
+    }
+    return filaEncontrada
+}
+
 fun colocarFicha(c:Int, ficha:Ficha) {
+    val f = obtenerFilaLibre(c)
+    if (f != NO_ENCONTRADO) {
+        tablero[f][c] = ficha
+    }
 }
 
 fun hacerMovimientoJugador() {
@@ -37,7 +67,7 @@ fun hacerMovimientoJugador() {
         val columna = readln().trim().toIntOrNull()
 
         if (columna != null) {
-            if (columValida(columna)) {
+            if (columnaValida(columna)) {
                 colocarFicha(columna, Ficha.JUGADOR)
                 movimientoValido = true
             }
@@ -51,6 +81,57 @@ fun hacerMovimientoJugador() {
     }
 }
 
+fun hacerMovimientoMaquina(dificultad: Dificultad) {
+    println("Turno del ordenador...")
+
+    var columnaElegida = NO_ENCONTRADO
+
+    if (dificultad == Dificultad.DIFICIL) {
+        columnaElegida = buscarMovimientoGanador(Ficha.MAQUINA)
+    }
+
+    if (columnaElegida == NO_ENCONTRADO) {
+        if (dificultad == Dificultad.DIFICIL || dificultad == Dificultad.MEDIO) {
+            columnaElegida = buscarMovimientoGanador(Ficha.JUGADOR)
+        }
+    }
+
+    if (columnaElegida == NO_ENCONTRADO) {
+        var columnaAzar: Int
+        var esValida = false
+
+        while (!esValida) {
+            columnaAzar = Random.nextInt(COLUMNAS)
+            if (columnaValida(columnaAzar)) {
+                columnaElegida = columnaAzar
+                esValida = true
+            }
+        }
+    }
+
+    colocarFicha(columnaElegida, Ficha.MAQUINA)
+}
+
+fun buscarMovimientoGanador(ficha: Ficha): Int {
+    var columnaGanadora = NO_ENCONTRADO
+
+    for (c in 0 until COLUMNAS) {
+        if (columnaGanadora == NO_ENCONTRADO && columnaValida(c)) {
+            val f = obtenerFilaLibre(c)
+
+            tablero[f][c] = ficha
+
+            if (comprobarVictoria(ficha)) {
+                columnaGanadora = c
+            }
+
+            tablero[f][c] = Ficha.VACIO
+        }
+    }
+
+    return columnaGanadora
+}
+
 fun imprimirTablero() {
     println()
 
@@ -58,17 +139,61 @@ fun imprimirTablero() {
     for (c in 0 until COLUMNAS) {
         print(" $c  ")
     }
-    println("\n+---------------------------+")
+    println("\n$AZUL+---------------------------+$RESET")
 
     for (f in 0 until FILAS) {
-        print("|")
+        print("$AZUL|$RESET")
         for (c in 0 until COLUMNAS) {
-            print("${tablero[f][c]}|")
+            print("${tablero[f][c]}$AZUL|$RESET")
         }
         println()
     }
 
-    println("+---------------------------+")
+    println("$AZUL+---------------------------+$RESET")
+}
+
+fun comprobarVictoria(ficha: Ficha): Boolean {
+    // HORIZONTAL (-)
+    for (f in 0 until FILAS) {
+        for (c in 0 until COLUMNAS - 3) {
+            if (tablero[f][c] == ficha &&
+                tablero[f][c+1] == ficha &&
+                tablero[f][c+2] == ficha &&
+                tablero[f][c+3] == ficha) return true
+        }
+    }
+
+    // VERTICAL (|)
+    for (f in 0 until FILAS - 3) {
+        for (c in 0 until COLUMNAS) {
+            if (tablero[f][c] == ficha &&
+                tablero[f+1][c] == ficha &&
+                tablero[f+2][c] == ficha &&
+                tablero[f+3][c] == ficha) return true
+        }
+    }
+
+    // DIAGONAL ASCENDENTE (/)
+    for (f in 3 until FILAS) {
+        for (c in 0 until COLUMNAS - 3) {
+            if (tablero[f][c] == ficha &&
+                tablero[f-1][c+1] == ficha &&
+                tablero[f-2][c+2] == ficha &&
+                tablero[f-3][c+3] == ficha) return true
+        }
+    }
+
+    // DIAGONAL DESCENDENTE (\)
+    for (f in 0 until FILAS - 3) {
+        for (c in 0 until COLUMNAS - 3) {
+            if (tablero[f][c] == ficha &&
+                tablero[f+1][c+1] == ficha &&
+                tablero[f+2][c+2] == ficha &&
+                tablero[f+3][c+3] == ficha) return true
+        }
+    }
+
+    return false
 }
 
 fun main() {
@@ -99,7 +224,32 @@ fun main() {
 
     println("Estupendo. Has elegido nivel $dificultadSeleccionada")
 
-    imprimirTablero()
+    var turnoJugador = true
+    var juegoTerminado = false
 
-    hacerMovimientoJugador()
+    while (!juegoTerminado) {
+        imprimirTablero()
+
+        if (turnoJugador) {
+            hacerMovimientoJugador()
+        }
+        else {
+            hacerMovimientoMaquina(dificultadSeleccionada)
+        }
+
+        val fichaActual = if (turnoJugador) Ficha.JUGADOR else Ficha.MAQUINA
+
+        if (comprobarVictoria(fichaActual)) {
+            imprimirTablero()
+            if (turnoJugador) {
+                println("¡Enhorabuena, has ganado!")
+            }
+            else {
+                println("Ha ganado el ordenador")
+            }
+            juegoTerminado = true
+        }
+
+        turnoJugador = !turnoJugador
+    }
 }
