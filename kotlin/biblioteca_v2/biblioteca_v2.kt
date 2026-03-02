cbfcf4d5808619a2d62fe11f774e670c580f8234
fernando-ruiz-rico <fernando@iessanvicente.com>
2026-03-02 08:43:49 +0100

    Funciones introducción de campos biblioteca v2

--- kotlin/biblioteca_v2/biblioteca_v2.kt
+++ kotlin/biblioteca_v2/biblioteca_v2.kt
@@ -1,3 +1,21 @@
-fun main() {
+fun pedirTextoValido(mensaje: String) : String {
+    while(true) {
+        print(mensaje)
+        val entrada = readln().trim()
+        if (entrada.isNotEmpty()) return entrada
+        println("Error: El campo no puede estar vacío.")
+    }
+}
 
+fun pedirIdValido() : String {
+    while(true) {
+        print("ID del Material (ej. LIB-001): ")
+        val entrada = readln().trim().uppercase()
+        if (entrada.matches(Regex("[A-Z]{3}-[0-9]{3}"))) return entrada
+        println("Error: Debes introducir 3 letras, un guión y 3 números.")
+    }
+}
+
+fun main() {
+    println(pedirIdValido())
 }
