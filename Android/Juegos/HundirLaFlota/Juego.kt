9ef09103c6910f2df18067e421440ad203ecaf39
fernando-ruiz-rico <fernando@iessanvicente.com>
2026-03-10 10:02:36 +0100

    Ya se colocan los barcos en el juego de hundir la flota versión Android

--- Android/Juegos/HundirLaFlota/Juego.kt
+++ Android/Juegos/HundirLaFlota/Juego.kt
@@ -1,3 +1,102 @@
 package com.example.myapplication
 
 import kotlin.random.Random
+
+enum class TipoBarco(val longitud: Int) {
+    PORTAAVIONES(5),
+    ACORAZADO(4),
+    CRUCERO(3),
+    SUBMARINO(3),
+    DESTRUCTOR(2)
+}
+
+enum class EstadoCasilla(val simbolo:String) {
+    AGUA("🟦"),
+    BARCO("🚢"),
+    TOCADO("💥"),
+    FALLO("⚪");
+
+    override fun toString(): String = simbolo
+}
+
+class Juego {
+    val DIMENSION = 10
+    val MUNICION_MAXIMA = 50
+
+    var oceano = MutableList(DIMENSION) {
+        MutableList(DIMENSION) {
+            EstadoCasilla.AGUA
+        }
+    }
+    val impactosNecesarios = TipoBarco.values().sumOf({ it.longitud })
+
+    var aciertos = 0
+    var misilesRestantes = MUNICION_MAXIMA
+    var juegoTerminado = false
+
+    var mensaje = "Pulsa iniciar para jugar"
+
+    fun iniciarPartida() {
+        oceano = MutableList(DIMENSION) { MutableList(DIMENSION) { EstadoCasilla.AGUA } }
+        colocarFlotaCompleta()
+        aciertos = 0
+        misilesRestantes = MUNICION_MAXIMA
+        juegoTerminado = false
+        mensaje = "Toca una casilla"
+    }
+
+    fun esPosicionValida(fila:Int, columna:Int, longitud:Int, horizontal:Boolean): Boolean {
+        val anchoBarco = if (horizontal) longitud else 1
+        val altoBarco = if (horizontal) 1 else longitud
+
+        if (fila + altoBarco > DIMENSION || columna + anchoBarco > DIMENSION) {
+            return false
+        }
+
+        val filaInicio = (fila - 1).coerceAtLeast(0)
+        val columnaInicio = (columna - 1).coerceAtLeast(0)
+        val filaFin = (fila + altoBarco).coerceAtMost(DIMENSION - 1)
+        val columnaFin = (columna + anchoBarco).coerceAtMost(DIMENSION - 1)
+
+        for (i in filaInicio..filaFin) {
+            for (j in columnaInicio..columnaFin) {
+                if (oceano[i][j] != EstadoCasilla.AGUA) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    fun colocarBarcoEnMatriz(fila:Int, columna:Int, longitud:Int, horizontal:Boolean) {
+        for (i in 0 until longitud) {
+            if (horizontal) {
+                oceano[fila][columna + i] = EstadoCasilla.BARCO
+            }
+            else {
+                oceano[fila + i][columna] = EstadoCasilla.BARCO
+            }
+        }
+    }
+
+    fun colocarBarcoAleatorio(barco: TipoBarco) {
+        var colocado = false
+        while(!colocado) {
+            val fila = Random.nextInt(DIMENSION)
+            val columna = Random.nextInt(DIMENSION)
+            val horizontal = Random.nextBoolean()
+
+            if (esPosicionValida(fila, columna, barco.longitud, horizontal)) {
+                colocarBarcoEnMatriz(fila, columna, barco.longitud, horizontal)
+                colocado = true
+            }
+        }
+    }
+
+    fun colocarFlotaCompleta() {
+        for (barco in TipoBarco.values()) {
+            colocarBarcoAleatorio(barco)
+        }
+    }
+}
--- Android/Juegos/HundirLaFlota/MainActivity.kt
+++ Android/Juegos/HundirLaFlota/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -30,4 +31,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaHundirLaFlota() {
+    val motorJuego = remember { Juego() }
+    var refrescar by remember { mutableIntStateOf(0) }
+
+    motorJuego.iniciarPartida()
+    refrescar++
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            for (f in 0 until motorJuego.DIMENSION) {
+                Row {
+                    for (c in 0 until motorJuego.DIMENSION) {
+                        val estado = motorJuego.oceano[f][c]
+                        var simbolo = estado.simbolo
+                        /*if (estado == EstadoCasilla.BARCO && !motorJuego.juegoTerminado) {
+                            simbolo = EstadoCasilla.AGUA.simbolo
+                        }*/
+                        Text(simbolo, fontSize = 18.sp)
+                    }
+                }
+            }
+        }
+    }
+
+    Text(text = "", modifier = Modifier.size(if(refrescar > 0) 0.dp else 0.dp))
 }
